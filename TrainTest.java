1959aba60a6e1f9c7d8069b4af8901c7ac331ea3
Abigail Westerlund <awesterlund@smith.edu>
2025-03-09 15:53:31 -0400

    Completed A6

--- TrainTest.java
+++ TrainTest.java
@@ -6,55 +6,103 @@ public class TrainTest {
     // Engine Tests
     @Test
     public void testEngineConstructor() {
-        fail();
+        Engine x = new Engine(FuelType.ELECTRIC, 20, 100);
+
+        assertEquals(FuelType.ELECTRIC, x.getFuelType());
+        assertEquals(20.0, x.getCurrentFuel(), 0.001);
+        assertEquals(100.0, x.getMaxFuel(), 0.001);
     }
 
     @Test
     public void testEngineGo() {
-        fail();
+        Engine x = new Engine(FuelType.ELECTRIC, 20, 100);
+        boolean result = x.go();
+
+        assertEquals(10.0, x.getCurrentFuel(), 0.001);
+        assertTrue(result);
     }
 
     // Car Tests
     @Test
     public void testCarAddPassenger() {
-        fail();
+        Passenger x = new Passenger("Abi");
+        Car c = new Car(10);
+        boolean result = c.addPassenger(x);
+
+        assertTrue(result);
     }
 
     @Test
     public void testCarRemovePassenger() {
-        fail();
+        Passenger x = new Passenger("Abi");
+        Car c = new Car(10);
+        c.addPassenger(x);
+        boolean result = c.removePassenger(x);
+
+        assertTrue(result);
     }
 
     // Passenger Tests
     @Test
     public void testPassengerBoardCarWithSpace() {
-        fail();
+        Car c = new Car(10);
+        Passenger x = new Passenger("Abi");
+        boolean result = x.boardCar(c);
+
+        assertTrue(result);
     }
 
     @Test
     public void testPassengerBoardCarFull() {
-        fail();
+        Car c = new Car(0);
+        Passenger x = new Passenger("Abi");
+        boolean result = x.boardCar(c);
+
+        assertFalse(result);
     }
 
     // Train Tests
     @Test
     public void testTrainConstructor() {
-        fail();
+        Train t = new Train(FuelType.ELECTRIC, 100, 3, 100);
+
+        assertNotNull(t.getCar(0));
+        assertNotNull(t.getCar(1));
+        assertNotNull(t.getCar(2));
     }
 
     @Test
     public void testTrainPassengerCount() {
-        fail();
+        Passenger x = new Passenger("Abi");
+        Train t = new Train(FuelType.ELECTRIC, 100, 3, 100);
+        
+        t.getCar(1).addPassenger(x);
+        assertEquals(1, t.getMaxCapacity() - t.seatsRemaining());
+
+        t.getCar(1).removePassenger(x);
+        assertEquals(0, t.getMaxCapacity() - t.seatsRemaining());
+
     }
 
     @Test
     public void testTrainGetCar() {
-        fail();
+        Train t = new Train(FuelType.ELECTRIC, 100, 3, 100);
+
+        Car car1 = t.getCar(0);
+        assertNotNull(car1);
+
+        Car car2 = t.getCar(1);
+        assertNotNull(car2);
+
+        Car car3 = t.getCar(2);
+        assertNotNull(car3);
     }
 
     @Test
-    public void testTrainPrintManifest() {
-        fail();
+    public void testTrainPrintManifest() { // I couldn't figure out how to go about this, so I will have to go to TA or office hours before I resubmit it
+        Train t = new Train(FuelType.ELECTRIC, 100, 3, 100);
+
+        t.printManifest();
     }
     
 }
--- reflection.md
+++ reflection.md
@@ -2,3 +2,5 @@ Use this file to record your reflection on this assignment.
 
 What are your initial impressions of how `Unit Testing` affects your programming?
 What worked, what didn't, what advice would you give someone taking this course in the future?
+
+I liked learning how to implement unit testing because it seemed like a more foolproof way to figure out if my code is working how I intended, without simply running the whole thing. Also because I could just test a very small piece of code instead of the entire file/class. I had a relativly easy time at first creating the unit tests. When I was writing the engine and car class tests, and I was able to use pretty similar methods to the example that we did in class and the slides. When I got to the train unit tests, I kept getting error messages and after trying to fix those issues a bunch of times, I'm pretty sure that my train class itself has errors. So hopefully when you try using my tests with the correct code it will compile without errors.
